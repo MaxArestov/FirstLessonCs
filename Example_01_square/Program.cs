9582326cba8dc04850b1a8540d9ee2191e141747
maxim arestov <aresmax1993@gmail.com>
2022-10-16 11:12:42 +0300

    Added example 01 square

---
+++ Example_01_square/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Программа для возведения числа в квадрат");
+Console.Write("Введите число, которое хотите возвести в квадрат: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.WriteLine(square);
