51d66bdbfd085c9be6e4c31ea5adbbd5672bfe85
maxim arestov <aresmax1993@gmail.com>
2022-10-16 12:18:48 +0300

    Added switch to Week

--- Example_03_Week/Program.cs
+++ Example_03_Week/Program.cs
@@ -1,31 +1,27 @@
 ﻿Console.Clear();
-Console.Write("Введите число от 1 до 7 :");
+Console.Write("Введите число от 1 до 7: ");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number == 1)
+switch (number)
 {
-    Console.WriteLine("Число соответствует понедельнику.");
-}
-if (number == 2)
-{
-    Console.WriteLine("Число соответствует вторнику.");
-}
-if (number == 3)
-{
-    Console.WriteLine("Число соответствует среде.");
-}
-if (number == 4)
-{
-    Console.WriteLine("Число соответствует четвергу.");
-}
-if (number == 5)
-{
-    Console.WriteLine("Число соответствует пятнице.");
-}
-if (number == 6)
-{
-    Console.WriteLine("Число соответствует субботе.");
-}
-if (number == 7)
-{
-    Console.WriteLine("Число соответствует воскресенью.");
+    case 1:
+    Console.WriteLine("Число соответствует понедельнику");
+    break;
+    case 2:
+    Console.WriteLine("Число соответствует вторнику");
+    break;
+    case 3:
+    Console.WriteLine("Число соответствует среде");
+    break;
+    case 4:
+    Console.WriteLine("Число соответствует четвергу");
+    break;
+    case 5:
+    Console.WriteLine("Число соответствует пятнице");
+    break;
+    case 6:
+    Console.WriteLine("Число соответствует субботе");
+    break;
+    case 7:
+    Console.WriteLine("Число соответствует воскресенью");
+    break;
 }
