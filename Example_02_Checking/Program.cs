130f247ec7e234c93d6223fc1934397ec04a003f
maxim arestov <aresmax1993@gmail.com>
2022-10-16 11:33:16 +0300

    Added Checking

---
+++ Example_02_Checking/Program.cs
@@ -0,0 +1,13 @@
+﻿int numberA = new Random().Next(1, 26);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 6);
+Console.WriteLine(numberB);
+int square = numberB * numberB;
+if (square == numberA)
+{
+    Console.WriteLine("Совпало!");
+}
+else
+{
+    Console.WriteLine("Не получилось. Попробуй заново.");
+}
